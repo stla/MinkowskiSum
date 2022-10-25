4dbe80c16aaff5409e695a529564161a74fd26ee
stla <laurent_step@yahoo.fr>
2022-10-25 14:54:40 +0200

    doc septuaginta

---
+++ R/data-septuaginta.R
@@ -0,0 +1,8 @@
+#' @title Septuaginta
+#'
+#' @description A mesh of the Septuaginta, also known as Leonardo da Vinci's 
+#'   72-sided sphere or Campanus's sphere. This is a convex polyhedra with 
+#'   62 vertices and 72 faces.
+#'
+#' @format A list with two fields: \code{vertices} and \code{faces}.
+"septuaginta"
